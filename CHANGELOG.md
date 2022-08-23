15dce2b8427e55d1f86db8c5bdd5b32ba72583b9
liu xm <gzyx0704@foxmail.com>
2022-08-23 17:21:49 +0800

    Add comment option

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+- Add comment option
+
 ## 0.0.1
 
 * Initial release of the annotation package.
--- lib/src/config.dart
+++ lib/src/config.dart
@@ -4,8 +4,11 @@
 /// @date  2022-08-22 17:04
 ///
 class DefaultConfig {
-  const DefaultConfig({required this.path});
+  const DefaultConfig({required this.path, this.hasComment = false});
 
   /// path
   final String path;
+
+  /// has comment
+  final bool hasComment;
 }
